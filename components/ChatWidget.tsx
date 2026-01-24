20168cf9eb4b16240b7e366500874b6de3f0497a
adil ghazali <aghazali34@gmail.com>
2026-01-24 09:29:10 -0600

    fix: increase z-index of chat widget to ensure visibility

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -142,7 +142,7 @@ export default function ChatWidget() {
 
     return (
         <>
-            <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-4">
+            <div className="fixed bottom-6 right-6 z-[9999] flex flex-col items-end gap-4">
                 <AnimatePresence>
                     {isOpen && (
                         <motion.div

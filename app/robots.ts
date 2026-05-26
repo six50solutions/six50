531343ce40980b9014e4b5eaecfd7bdf0e936370
six50solutions <adil.ghazali@six50.io>
2026-05-26 18:49:06 -0500

    seo: add robots.ts with AI crawler rules

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,17 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
-    sitemap: 'https://six50.io/sitemap.xml',
-  }
+    rules: [
+      { userAgent: "*",               allow: "/", disallow: ["/login", "/api/", "/_next/"] },
+      { userAgent: "GPTBot",          allow: "/", disallow: ["/login", "/api/"] },
+      { userAgent: "ClaudeBot",       allow: "/", disallow: ["/login", "/api/"] },
+      { userAgent: "Google-Extended", allow: "/", disallow: ["/login", "/api/"] },
+      { userAgent: "PerplexityBot",   allow: "/", disallow: ["/login", "/api/"] },
+      { userAgent: "FacebookBot",     allow: "/", disallow: ["/login", "/api/"] },
+      { userAgent: "CCBot",           allow: "/" },
+    ],
+    sitemap: "https://six50.io/sitemap.xml",
+    host: "https://six50.io",
+  };
 }

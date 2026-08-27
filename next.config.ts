4fe0c9e13825eb50c7488097bd2123a8aa358b15
six50solutions <adil.ghazali@six50.io>
2026-08-27 16:23:28 -0500

    Update redirects and rewrites in next.config.ts
    Added a new rewrite rule for the '/risk-analyzer/thanks' path.

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-          return [
-            {
-                      source: '/book',
-                      destination: 'https://calendly.com/adil-ghazali-six50',
-                      permanent: false,
-            },
-                ];
-    },
-    async rewrites() {
-          return [
-            {
-                      source: '/risk-analyzer',
-                      destination: '/risk-analyzer.html',
-            },
-                ];
-    },
+  async redirects() {
+    return [
+      {
+        source: '/book',
+        destination: 'https://calendly.com/adil-ghazali-six50',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/risk-analyzer',
+        destination: '/risk-analyzer.html',
+      },
+      {
+        source: '/risk-analyzer/thanks',
+        destination: '/risk-analyzer-thanks.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
